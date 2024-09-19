40423280108884f1e1979386fb1f656e06fff593
Aurel Bieri <bieri.aurel@gmail.com>
2024-09-19 10:47:26 +0200

    linter test

---
+++ eslint.js
@@ -0,0 +1,20 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+export default [
+  {
+    languageOptions: {
+      globals: globals.node, 
+      ecmaVersion: 2021,
+      sourceType: 'module'
+    },
+  },
+  pluginJs.configs.recommended, 
+  {
+    rules: {
+      'no-unused-vars': 'error',    
+      'eqeqeq': ['error', 'always'], 
+      'no-console': 'off',           
+    },
+  },
+];
--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import chalk from "chalk"
-
 console.log(chalk.blue("It works"))
+
+let myUnusedVariable = 5; 
+
+if (myUnusedVariable == 5) { 
+  console.log("This is a test.");
+}
